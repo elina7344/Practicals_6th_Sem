6ef394ad256ccfae8096344b2b3a7520497a3a73
Elina Singh <71961061+elina7344@users.noreply.github.com>
2023-04-20 10:42:56 +0530

    Create EXP_4.py

---
+++ AI/EXP_4.py
@@ -0,0 +1,256 @@
+#Best First Search
+
+# A Node class for GBFS Pathfinding
+class Node:
+    def __init__(self, v, weight):
+        self.v=v
+        self.weight=weight
+
+# pathNode class will help to store
+# the path from src to dest.
+class pathNode:
+    def __init__(self, node, parent):
+        self.node=node
+        self.parent=parent
+
+# Function to add edge in the graph.
+def addEdge(u, v, weight):
+    # Add edge u -> v with weight weight.
+    adj[u].append(Node(v, weight))
+
+
+# Declaring the adjacency list
+adj = []
+# Greedy best first search algorithm function
+def GBFS(h, V, src, dest):
+    """ 
+    This function returns a list of 
+    integers that denote the shortest
+    path found using the GBFS algorithm.
+    If no path exists from src to dest, we will return an empty list.
+    """
+    # Initializing openList and closeList.
+    openList = []
+    closeList = []
+
+    # Inserting src in openList.
+    openList.append(pathNode(src, None))
+
+    # Iterating while the openList 
+    # is not empty.
+    while (openList):
+
+        currentNode = openList[0]
+        currentIndex = 0
+        # Finding the node with the least 'h' value
+        for i in range(len(openList)):
+            if(h[openList[i].node] < h[currentNode.node]):
+                currentNode = openList[i]
+                currentIndex = i
+
+        # Removing the currentNode from 
+        # the openList and adding it in 
+        # the closeList.
+        openList.pop(currentIndex)
+        closeList.append(currentNode)
+        
+        # If we have reached the destination node.
+        if(currentNode.node == dest):
+            # Initializing the 'path' list. 
+            path = []
+            cur = currentNode
+
+            # Adding all the nodes in the 
+            # path list through which we have
+            # reached to dest.
+            while(cur != None):
+                path.append(cur.node)
+                cur = cur.parent
+            
+
+            # Reversing the path, because
+            # currently it denotes path
+            # from dest to src.
+            path.reverse()
+            return path
+        
+
+        # Iterating over adjacents of 'currentNode'
+        # and adding them to openList if 
+        # they are neither in openList or closeList.
+        for node in adj[currentNode.node]:
+            for x in openList:
+                if(x.node == node.v):
+                    continue
+            
+            for x in closeList:
+                if(x.node == node.v):
+                    continue
+            
+            openList.append(pathNode(node.v, currentNode))
+
+    return []
+
+# Driver Code
+""" Making the following graph
+             src = 0
+            / | \
+           /  |  \
+          1   2   3
+         /\   |   /\
+        /  \  |  /  \
+        4   5 6 7    8
+               /
+              /
+            dest = 9
+"""
+# The total number of vertices.
+V = 10
+## Initializing the adjacency list
+for i in range(V):
+    adj.append([])
+
+addEdge(0, 1, 2)
+addEdge(0, 2, 1)
+addEdge(0, 3, 10)
+addEdge(1, 4, 3)
+addEdge(1, 5, 2)
+addEdge(2, 6, 9)
+addEdge(3, 7, 5)
+addEdge(3, 8, 2)
+addEdge(7, 9, 5)
+
+# Defining the heuristic values for each node.
+h = [20, 22, 21, 10, 25, 24, 30, 5, 12, 0]
+path = GBFS(h, V, 0, 9)
+for i in range(len(path) - 1):
+    print(path[i], end = " -> ")
+
+print(path[(len(path)-1)])
+
+
+#OUTPUT BFS:
+'''
+0 -> 3 -> 7 -> 9
+'''
+
+
+#A*Search
+
+from collections import deque
+
+class Graph:
+    # example of adjacency list (or rather map)
+    # adjacency_list = {
+    # 'A': [('B', 1), ('C', 3), ('D', 7)],
+    # 'B': [('D', 5)],
+    # 'C': [('D', 12)]
+    # }
+
+    def __init__(self, adjacency_list):
+        self.adjacency_list = adjacency_list
+
+    def get_neighbors(self, v):
+        return self.adjacency_list[v]
+
+    # heuristic function with equal values for all nodes
+    def h(self, n):
+        H = {
+            'A': 1,
+            'B': 1,
+            'C': 1,
+            'D': 1
+        }
+
+        return H[n]
+
+    def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v;
+
+            if n == None:
+                print('Path does not exist!')
+                return None
+
+            # if the current node is the stop_node
+            # then we begin reconstructin the path from it to the start_node
+            if n == stop_node:
+                reconst_path = []
+
+                while parents[n] != n:
+                    reconst_path.append(n)
+                    n = parents[n]
+
+                reconst_path.append(start_node)
+
+                reconst_path.reverse()
+
+                print('Path found: {}'.format(reconst_path))
+                return reconst_path
+
+            # for all neighbors of the current node do
+            for (m, weight) in self.get_neighbors(n):
+                # if the current node isn't in both open_list and closed_list
+                # add it to open_list and note n as it's parent
+                if m not in open_list and m not in closed_list:
+                    open_list.add(m)
+                    parents[m] = n
+                    g[m] = g[n] + weight
+
+                # otherwise, check if it's quicker to first visit n, then m
+                # and if it is, update parent data and g data
+                # and if the node was in the closed_list, move it to open_list
+                else:
+                    if g[m] > g[n] + weight:
+                        g[m] = g[n] + weight
+                        parents[m] = n
+
+                        if m in closed_list:
+                            closed_list.remove(m)
+                            open_list.add(m)
+
+            # remove n from the open_list, and add it to closed_list
+            # because all of his neighbors were inspected
+            open_list.remove(n)
+            closed_list.add(n)
+
+        print('Path does not exist!')
+        
+        return None
+        
+        
+adjacency_list = {
+    'A': [('B', 1), ('C', 3), ('D', 7)],
+    'B': [('D', 5)],
+    'C': [('D', 12)]
+}
+graph1 = Graph(adjacency_list)
+graph1.a_star_algorithm('A', 'D')
+
+#OUTPUT A* :
+'''
+Path found: ['A', 'B', 'D']
+'''
+
