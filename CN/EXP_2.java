7492360b40a714bd9c438579a88f389871fe5f66
Elina Singh <71961061+elina7344@users.noreply.github.com>
2023-04-26 21:51:58 +0530

    Update EXP_2.java

--- CN/EXP_2.java
+++ CN/EXP_2.java
@@ -1,3 +1,25 @@
+//Ping is a simple command that can test the reachability of a device on the network.
+
+//Traceroute is a command you use to 'trace' the 
+/*route that a packet takes when traveling to its destination. 
+    It's useful for tracing network problems, 
+    discovering where connections fail, and tracking down latency problems.*/
+
+/*PING ALGORITHM
+
+Step 1:	Start the program. 
+
+Step 2:	Import net and packages. 
+
+Step 3:	Get the IP address 
+
+Step 4:	Ping the remote server using Ping Command 
+
+Step 5:	The Packet statistics of the pinged server is displayed */
+
+
+
+
 //Ping
 package EXP_2;
 import java.io.*;
@@ -27,6 +49,18 @@ java.util.Date date=new java.util.Date();
 System.out.println(date);
 }
 }
+
+/*TRACEROUTE ALGO
+
+Step 1:	Start the program. 
+
+Step 2:	Import net and packages. 
+
+Step 3:	Get the IP address 
+
+Step 4:	Traceroute the remote server using traceroute Command 
+
+Step 5:	The number of max hopes and byte packets are displayed.*/
 
 //traceroute
 package EXP_2;
